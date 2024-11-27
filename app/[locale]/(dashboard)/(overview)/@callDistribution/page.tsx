c2668abed5a385446e4b09f0553380a728c743e6
abdelhamied <abdelhamied403@gmail.com>
2024-11-27 17:21:23 +0200

    charts

--- app/[locale]/(dashboard)/(overview)/@callDistribution/page.tsx
+++ app/[locale]/(dashboard)/(overview)/@callDistribution/page.tsx
@@ -1,8 +1,40 @@
+"use client";
+
+import { Bar, BarChart } from "recharts";
+import { ChartConfig, ChartContainer } from "@/components/ui/chart";
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "#2563eb",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "#60a5fa",
+  },
+} satisfies ChartConfig;
+
 const CallDistribution = () => {
+  const chartData = [
+    { month: "January", desktop: 186, mobile: 80 },
+    { month: "February", desktop: 305, mobile: 200 },
+    { month: "March", desktop: 237, mobile: 120 },
+    { month: "April", desktop: 73, mobile: 190 },
+    { month: "May", desktop: 209, mobile: 130 },
+    { month: "June", desktop: 214, mobile: 140 },
+  ];
+
   return (
     <div className="page" id="callDistribution">
       <div className="border-gray-200 bg-white shadow-md p-4 border rounded">
         <p>Call Distribution</p>
+
+        <ChartContainer config={chartConfig} className="w-full min-h-[200px]">
+          <BarChart accessibilityLayer data={chartData}>
+            <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+            <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+          </BarChart>
+        </ChartContainer>
       </div>
     </div>
   );
