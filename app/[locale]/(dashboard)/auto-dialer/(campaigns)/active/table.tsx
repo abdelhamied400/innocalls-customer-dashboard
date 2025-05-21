dd0d9d19c41937cf513d8ca775e15411003d5e46
Abdelhamied <Abdelhamied403@gmail.com>
2025-05-21 12:46:20 +0300

    fix: convert pagination parameters to strings for consistent filter updates

--- app/[locale]/(dashboard)/auto-dialer/(campaigns)/active/table.tsx
+++ app/[locale]/(dashboard)/auto-dialer/(campaigns)/active/table.tsx
@@ -41,8 +41,8 @@ const ActiveCampaignsTable = () => {
       manualPagination
       onPaginationChange={({ pageSize, pageIndex }) => {
         updateFilters({
-          page: pageIndex + 1,
-          perPage: pageSize,
+          page: String(pageIndex + 1),
+          perPage: String(pageSize),
         });
       }}
       defaultPageIndex={pageIndex}
