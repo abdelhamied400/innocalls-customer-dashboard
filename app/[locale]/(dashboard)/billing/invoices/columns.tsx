4e69ec948223a7afeeae9b299941798be98abc2f
Abdelhamied <Abdelhamied403@gmail.com>
2025-05-25 16:56:24 +0300

    feat: add Invoices table with pagination, filtering, and data fetching

---
+++ app/[locale]/(dashboard)/billing/invoices/columns.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { Badge } from "@/components/ui/badge";
+import { ColumnDef } from "@tanstack/react-table";
+
+export type Invoice = {
+  currencyCode: string;
+  date: string;
+  email: string;
+  id: string;
+  number: string;
+  status: string;
+  total: number;
+};
+
+export const columns: ColumnDef<Invoice>[] = [
+  {
+    accessorKey: "refNumber",
+    header: "#Ref-No",
+  },
+  {
+    accessorKey: "date",
+    header: "Date",
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    accessorKey: "total",
+    header: "Total",
+    cell: ({ row }) => {
+      const total = row.getValue("total") as number;
+      const currencyCode = row.original.currencyCode as string;
+      return (
+        <div className="flex flex-col items-center w-min">
+          <span>{total}</span>
+          <span>{currencyCode}</span>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => {
+      const status = row.getValue("status") as string;
+      return (
+        <div className="flex items-center gap-2">
+          <Badge variant={status === "paid" ? "success" : "destructive"}>
+            {status}
+          </Badge>
+        </div>
+      );
+    },
+  },
+];
---
+++ app/[locale]/(dashboard)/billing/invoices/page.tsx
@@ -0,0 +1,11 @@
+import InvoicesTable from "./table";
+
+const Invoices = async () => {
+  return (
+    <div className="page" id="invoices">
+      <InvoicesTable />
+    </div>
+  );
+};
+
+export default Invoices;
