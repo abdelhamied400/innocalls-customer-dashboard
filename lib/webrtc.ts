301a552c741be02b6d93fa04da96aae7ab623045
Abdelhamied <Abdelhamied403@gmail.com>
2025-07-09 14:42:15 +0300

    fix: update import to use webrtcCountries for country code replacement

--- lib/webrtc.ts
+++ lib/webrtc.ts
@@ -1,7 +1,7 @@
-import { countries } from "@/constants/countries";
+import { webrtcCountries } from "@/constants/countries";
 
 export const replaceCountryCode = (number: string): string => {
-  const country = countries.find((c) => number.startsWith(c.code));
+  const country = webrtcCountries.find((c) => number.startsWith(c.code));
   if (country) {
     return number.replace(country.code, country.dialCode);
   }
