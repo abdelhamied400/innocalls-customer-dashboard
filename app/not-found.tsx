7b045e7c3787402989e3923392b1db549a98cceb
Abdelhamied <Abdelhamied403@gmail.com>
2025-09-07 14:40:38 +0300

    refactor: Simplify NotFound component and add translations for not found messages

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,36 +1,11 @@
 "use client";
-import { defaultLocale, LocaleSlug } from "@/i18n/config";
-import localFont from "next/font/local";
-import { NextFontWithVariable } from "next/dist/compiled/@next/font";
-import NotFoundContent from "@/containers/NotFoundContent";
-import { TranslationProvider } from "@/providers/TranslationProvider";
 
-const poppins = localFont({
-  src: "./fonts/Poppins.otf",
-  variable: "--font-poppins",
-  weight: "100 900",
-});
-const cairo = localFont({
-  src: "./fonts/Cairo.ttf",
-  variable: "--font-cairo",
-  weight: "100 900",
-});
-
-const fonts: Record<LocaleSlug, NextFontWithVariable> = {
-  en: poppins,
-  ar: cairo,
-};
+import FullPageError from "@/containers/FullPageError";
+import { useTranslations } from "@/providers/TranslationProvider";
 
 export default function NotFound() {
-  const locale = defaultLocale; // Use default locale for not found page
-
+  const t = useTranslations("notFound");
   return (
-    <html lang={locale}>
-      <body className={`${fonts[locale].variable} antialiased`}>
-        <TranslationProvider initialLocale={locale}>
-          <NotFoundContent />
-        </TranslationProvider>
-      </body>
-    </html>
+    <FullPageError status={404} title={t("title")} message={t("message")} />
   );
 }
