57bcb688fc17a18f6e17896fc9756a8a560f0f76
Aml132 <amal.kamel@businessvalley.com>
2025-09-25 17:26:45 +0300

    remove firstBreakColumn , Edit ExpendCell

--- components/ExpandCell.tsx
+++ components/ExpandCell.tsx
@@ -1,13 +1,26 @@
 import { Row } from "@tanstack/react-table";
 
+import { ChevronRightCircle, ChevronDownCircle } from "lucide-react";
+
 type ExpandCellProps<T> = {
   row: Row<T>;
 };
+
 const ExpandCell = <T,>({ row }: ExpandCellProps<T>) => {
   return (
     <div className="flex items-center justify-center">
       <button onClick={() => row.toggleExpanded()}>
-        {row.getIsExpanded() ? "👇" : "👉"}
+        {row.getIsExpanded() ? (
+          <ChevronDownCircle
+            size={16}
+            className="text-primary-500 hover:text-primary-600"
+          />
+        ) : (
+          <ChevronRightCircle
+            size={16}
+            className="text-gray-500 hover:text-gray-700"
+          />
+        )}
       </button>
     </div>
   );
