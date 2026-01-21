1c98247506cdfc3cdf154750ebe34dce18887a86
Abdelhamied <Abdelhamied403@gmail.com>
2026-01-21 17:06:08 +0200

    hotfix for mac devices

--- components/Webrtc/Call/CallSummaryModal.tsx
+++ components/Webrtc/Call/CallSummaryModal.tsx
@@ -6,6 +6,7 @@ import CallSummaryForm from "./CallSummaryForm";
 import { DialogDescription } from "@radix-ui/react-dialog";
 import { useTranslations } from "@/providers/TranslationProvider";
 import useAuthStore from "@/store/auth.slice";
+import { useEffect, useState } from "react";
 
 const CallSummaryModal = () => {
   const { data: session } = useSession();
@@ -16,11 +17,10 @@ const CallSummaryModal = () => {
     lastCall,
     clearLastCall,
   } = useWebrtcStore();
+  const t = useTranslations("webrtc.summary");
 
   if (session?.userType === "user" || !Organization?.enableAfterCallTags)
     return null;
-
-  const t = useTranslations("webrtc.summary");
 
   const onCallSummaryModalOpenChange = (open: boolean) => {
     setCallSummaryModalOpen(open);
@@ -34,7 +34,10 @@ const CallSummaryModal = () => {
       open={callSummaryModalOpen}
       onOpenChange={onCallSummaryModalOpenChange}
     >
-      <DialogContent className="max-h-[90vh] overflow-auto">
+      <DialogContent
+        className="max-h-[90vh] overflow-auto"
+        onCloseAutoFocus={(e) => e.preventDefault()}
+      >
         <DialogTitle>{t("title")}</DialogTitle>
         <DialogDescription className="text-sm text-muted-foreground">
           {lastCall?.from} &rarr; {lastCall?.to} |{" "}
@@ -44,12 +47,12 @@ const CallSummaryModal = () => {
             : `0 ${t("conclusion.duration.sec")}`}
           {lastCall?.status
             ? ` | ${t("conclusion.fields.status")}: ${t(
-                `conclusion.status.${lastCall.status.toLocaleLowerCase()}`
+                `conclusion.status.${lastCall.status.toLocaleLowerCase()}`,
               )}`
             : ""}
           {lastCall?.callDateTime
             ? ` | ${t("conclusion.fields.date")}: ${new Date(
-                lastCall.callDateTime
+                lastCall.callDateTime,
               ).toLocaleString()}`
             : ""}
         </DialogDescription>
