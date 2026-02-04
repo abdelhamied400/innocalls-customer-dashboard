3853ecb619997cca397fa4aeb205cff00796d170
Abdelhamied <Abdelhamied403@gmail.com>
2026-02-04 18:24:54 +0200

    feat: add mobile menu button to ResponsiveSidebar for improved navigation

--- components/ResponsiveSidebar.tsx
+++ components/ResponsiveSidebar.tsx
@@ -5,6 +5,8 @@ import AppSidebar from "@/components/AppSidebar";
 import useAppStore from "@/store/app.slice";
 import { useLayoutManager } from "@/hooks/use-layout-manager";
 import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const ResponsiveSidebar = () => {
   const { isSidebarOpen, toggleSidebar } = useAppStore();
@@ -22,6 +24,17 @@ const ResponsiveSidebar = () => {
   if (shouldUseSidebarSheet) {
     return (
       <>
+        {/* Mobile menu button */}
+        {!isSidebarOpen && (
+          <Button
+            variant="outline"
+            size="icon"
+            onClick={toggleSidebar}
+            className="fixed top-28 -start-4 z-50 shadow-md translate-x-0 transition-transform duration-200 hover:translate-x-2 rtl:hover:-translate-x-2"
+          >
+            <MenuIcon />
+          </Button>
+        )}
         <Sheet
           open={isSidebarOpen}
           onOpenChange={(e) => {
