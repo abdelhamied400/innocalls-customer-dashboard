a18c37df5815a4ccaa50dd6fb1246a27b1ae8bfa
aml <amal.kamel@businessvalley.com>
2026-03-26 15:42:10 +0200

    feat: update auto dialer sidebar item to be enabled and marked as new

--- components/AppSidebar.tsx
+++ components/AppSidebar.tsx
@@ -136,9 +136,9 @@ const AppSidebar = () => {
               icon={<RingVolume />}
               title={t("navigation.autoDialer")}
               href={`/auto-dialer`}
-              disabled={true}
-              isNew={false}
-              isComingSoon={true}
+              disabled={false}
+              isNew={true}
+              isComingSoon={false}
             />
           </SidebarCollapsibleItem>
         )}
@@ -203,9 +203,9 @@ const AppSidebar = () => {
                 icon={<RingVolume />}
                 title={t("navigation.autoDialer")}
                 href={`/auto-dialer`}
-                disabled={true}
-                isNew={false}
-                isComingSoon={true}
+                disabled={false}
+                isNew={true}
+                isComingSoon={false}
               />
             )}
             <SidebarItem
